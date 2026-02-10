c55e0aaa2cdd2ff25f0d0b43cc13904c020f995b
Guilherme Rodrigues <guilhermerodriguesm23@gmail.com>
2026-02-10 16:10:44 -0300

    Commit inicial com a base do projeto

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-sky-200 min-h-screen w-full">
+      <h1 className="text-black text-4xl font-bold text-center p-4">Trivia</h1>
+    </div>
+  );
+}
+
+export default App;
